c693a149c714a675ffa03f5383017b9155a87fca
Reid Wilbur <reid.wilbur@gmail.com>
2013-10-29 16:17:50 -0500

    Adding some scaladoc

--- src/main/scala/com/wilb0t/flow/api/ExitPort.scala
+++ src/main/scala/com/wilb0t/flow/api/ExitPort.scala
@@ -1,5 +1,7 @@
 package com.wilb0t.flow.api
 
+/** The result of executing an [[com.wilb0t.api.Action]]
+  */
 abstract class ExitPort {
   def description: String
   override def toString: String = {
@@ -7,6 +9,10 @@ abstract class ExitPort {
   }
 }
 
+/** Since different contexts can exit with different [[com.wilb0t.api.ExitPort]]s
+  * parallel subflows have an implicit sync point at the end of the flow and 
+  * always return this exit port.
+  */
 case class ParSubFlowExit() extends ExitPort {
   val description = "Parallel subflow exit"
 }
